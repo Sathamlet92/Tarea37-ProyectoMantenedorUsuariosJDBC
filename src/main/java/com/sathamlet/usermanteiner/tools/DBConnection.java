8a68af756748926966ae785b4b67aa19bd37a669
Sathamlet <sathamlet@gmail.com>
2022-08-29 15:39:36 -0500

    NO DATA REVEAL

--- src/main/java/com/sathamlet/usermanteiner/tools/DBConnection.java
+++ src/main/java/com/sathamlet/usermanteiner/tools/DBConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static String stringConn = "jdbc:mysql://127.0.0.1:3306/Java_course?serverTimeZone=America/Mexico_City";
-    private static String userName = "root";
-    private static String password = "California_4010YA";
+    private static String userName = "omitted";
+    private static String password = "omitted";
     private static Connection connection;
 
     public static Connection getConnection() throws SQLException {
